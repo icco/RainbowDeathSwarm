8d44c3bb7b344e0471b341b573b7334bb68f928b
Nat Welch <nat@natwelch.com>
2011-01-29 03:58:43 -0800

    no love.

--- main.lua
+++ main.lua
@@ -83,13 +83,6 @@ function love.update(dt)
    world:update(dt)
 end
 
-function drawSimpleRect(obj)
-   local x1, y1, x2, y2, x3, y3, x4, y4 = obj.shape:getBoundingBox()
-   local w = x3 - x2
-   local h = y2 - y1
-   love.graphics.rectangle("fill", obj.body:getX() - (w / 2), obj.body:getY() - (h / 2), w, h)
-end
-
 function love.draw()
    -- convenience
    local gfx = love.graphics
