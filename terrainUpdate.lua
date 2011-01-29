d59ec2cfc98047f2305911f4b5a5b34cf7508338
Taylor Arnicar <MutantFrk@gmail.com>
2011-01-29 15:55:38 -0800

    Changes.

--- terrainUpdate.lua
+++ terrainUpdate.lua
@@ -1,9 +1,14 @@
 -- This function should be called whenever we need to delete the far left column, and draw a new right column.
 function updateTerrain()
+   local difficulty = math.floor(now/15)
+
+print("difficulty is " .. difficulty)
    -- If the offset counter has reached 2 million
    -- then copy the map back to index 1 and reset the counter.
 
    -- Clear out the left hand column (map[1+offset])
+   map[map["counter"]] = nil
+
    local colCount = (1 + map["counter"] + map["howLong"])
 
    -- Initialize this column to nothing
@@ -18,21 +23,21 @@ function updateTerrain()
       end
    end
 
-   local randomBlockCount = map[colCount-1]["NumOfBlocks"] + math.ceil((math.random()-0.5) * 4)
-   if randomBlockCount < 0 then
-      randomBlockCount = 0 
-   elseif randomBlockCount > 8 then
-      randomBlockCount = 8
-   end
+--   local randomBlockCount = map[colCount-1]["NumOfBlocks"] + math.ceil((math.random()-0.5) * 4)
+--   if randomBlockCount < 0 then
+--      randomBlockCount = 0 
+--   elseif randomBlockCount > 8 then
+--      randomBlockCount = 8
+--   end
 
    -- Generate a new far right column (map[1+offset + (numColums, AKA ARENA_WIDTH/map[howLong])
-   for rowCount = map.howHigh-2, map.howHigh-randomBlockCount, -1 do
-      local x = (map["boxw"] * colCount)
-      local y = (map["boxw"] * rowCount)
-      map[colCount][rowCount] = makeCube(map["boxw"], x, y)
-   end
+--   for rowCount = map.howHigh-2, map.howHigh-randomBlockCount, -1 do
+--      local x = (map["boxw"] * colCount)
+--      local y = (map["boxw"] * rowCount)
+--      map[colCount][rowCount] = makeCube(map["boxw"], x, y)
+--   end
 
-   map[colCount]["NumOfBlocks"] =  randomBlockCount
+--   map[colCount]["NumOfBlocks"] =  randomBlockCount
    
    -- Increment the offset counter by one
    map["counter"] = map["counter"]+1
