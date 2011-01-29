ccb688cbfdf77e82dbc0b348fd6d5a3e84290137
Katherine Blizard <kblizard@Zapdos.(none)>
2011-01-29 03:06:41 -0800

    added some comments

--- terrainUpdate.lua
+++ terrainUpdate.lua
@@ -1,1 +1,12 @@
---update function for the terrain goes here
+
+-- This function should be called whenever we need to delete the far left column, and draw a new right column.
+function updateTerrain()
+   -- If the offset counter has reached 2 million
+      -- then copy the map back to index 1 and reset the counter.
+
+   -- Clear out the left hand column (map[1+offset])
+   -- Generate a new far right column (map[1+offset + (numColums, AKA ARENA_WIDTH/map[howLong])
+
+   -- Increment the offset counter by one
+   map[counter] = map[counter]+1
+end
