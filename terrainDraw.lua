3a171ed3c942acf3a0260bba39dcc233c72eabd9
Nat Welch <nat@natwelch.com>
2011-01-29 00:12:51 -0800

    Kind of works, not really.

--- terrainDraw.lua
+++ terrainDraw.lua
@@ -1,1 +1,15 @@
 -- draw function for the terrain goes here
+
+function drawTerrain()
+   local gfx = love.graphics
+   local colCount = 0
+   local rowCount = 0
+
+   gfx.setColor(0, 255, 0)
+
+   for colCount = 1, 25, 1 do
+      for rowCount = 1, 13, 1 do
+         drawSimpleRect(map[colCount][rowCount])
+      end
+   end
+end
--- terrainLoad.lua
+++ terrainLoad.lua
@@ -4,26 +4,33 @@ map = {counter = 0 }
 
 -- Load function for the terrain
 function initTerrain()
-   local boxw = SCREEN_WIDTH / 24
+   local boxw = SCREEN_HEIGHT / 25
+   local colCount = 0
+   local rowCount = 0
 
-   for colCount = 1, colCount == 24, colCount + 1 do
+   for colCount = 1, 25, 1 do
       map[colCount] = {}
 
-      for rowCount = 1, rowCount == 24, rowCount + 1 do
-         map[colCount][rowCount] = makeCube(boxw, (boxw * colCount), (boxw * rowCount))
+      for rowCount = 1, 13, 1 do
+         print(' ', colCount, rowCount)
+         map[colCount][rowCount] = makeCube(boxw, (boxw * (colCount-1)), (boxw * (rowCount-1)))
       end
    end
 end
 
 -- Posx and Posy should be top left
 function makeCube(size, posx, posy)
+   local gfx = love.graphics
+   local phys = love.physics
    local ret = {}
 
+   print(size, posx, posy)
    -- love.physics.newBody( world, x, y, mass, inertia )
    ret.body = phys.newBody(world, posx+(size/2), posy+(size/2), 0 ,0)
 
    -- love.physics.newRectangleShape( body, x, y, w, h, angle )
    ret.shape = phys.newRectangleShape(ret.body, posx, posy, size, size, 0)
    -- ret.shape = phys.newRectangleShape()
+
    return ret
 end
