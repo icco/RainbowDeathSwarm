a2039af6f2a3d24f59246a6a472adbdaeff1967b
Nat Welch <nat@natwelch.com>
2011-01-29 04:59:41 -0800

    Fix the random nat.

--- swarmDraw.lua
+++ swarmDraw.lua
@@ -3,11 +3,6 @@ local gfx = love.graphics
 
 function swarmDrawFunction()
    gfx.setColor(255,255,255)
-   gfx.draw(ball.img, 
-      ball.body:getX(), 
-      ball.body:getY(), 
-      ball.body:getAngle(), 1, 1, SQUIRREL_RADIUS, SQUIRREL_RADIUS)
-
    for count = 1, #Swarm do
       local squ = Swarm[count]
       gfx.draw(squ.img, squ.body:getX(), squ.body:getY(), 0, 1, 1, SQUIRREL_RADIUS, SQUIRREL_RADIUS)
